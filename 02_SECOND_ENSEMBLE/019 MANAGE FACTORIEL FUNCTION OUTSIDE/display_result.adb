58296f3b6648e489c37dfa217ff130cf70581056
Merzouk MENHOUR <merzouk@gmail.com>
2023-06-30 01:12:38 +0200

    add example

--- 02_SECOND_ENSEMBLE/019 MANAGE FACTORIEL FUNCTION OUTSIDE/display_result.adb
+++ 02_SECOND_ENSEMBLE/019 MANAGE FACTORIEL FUNCTION OUTSIDE/display_result.adb
@@ -1,7 +1,7 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
    
-   procedure display_result(a: integer; b : integer ) is
+   procedure display_result(a: integer; b :integer ) is
    begin
       Put_Line ("Factorial of "
                 & Integer'Image (a)
