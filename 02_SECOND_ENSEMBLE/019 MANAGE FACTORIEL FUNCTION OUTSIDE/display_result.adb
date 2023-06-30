1c9b4616c68eba92a7602118eeff8ffa2c306a13
Merzouk MENHOUR <merzouk@gmail.com>
2023-06-30 09:30:52 +0200

    update examples

--- 02_SECOND_ENSEMBLE/019 MANAGE FACTORIEL FUNCTION OUTSIDE/display_result.adb
+++ 02_SECOND_ENSEMBLE/019 MANAGE FACTORIEL FUNCTION OUTSIDE/display_result.adb
@@ -3,8 +3,15 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
    
    procedure display_result(a: integer; b : integer ) is
    begin
-      Put_Line ("Factorial of "
-                & Integer'Image (a)
-                & " is "
-                & Integer'Image (b));
+      Put_Line ( if (a <= 9) then 
+					"Factorial of "
+					& Integer'Image (a)
+					& "  is  "
+					& Integer'Image (b)
+				else
+					"Factorial of "
+					& Integer'Image (a)
+					& " is  "
+					& Integer'Image (b)
+				);
    end display_result;
