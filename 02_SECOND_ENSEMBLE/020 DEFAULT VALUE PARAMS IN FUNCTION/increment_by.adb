638fc5314d20823fb7dda5290a1f6c5d2d0f6f1b
Merzouk MENHOUR <merzouk@gmail.com>
2023-06-30 10:21:49 +0200

    add examples

---
+++ 02_SECOND_ENSEMBLE/020 DEFAULT VALUE PARAMS IN FUNCTION/increment_by.adb
@@ -0,0 +1,6 @@
+function increment_by
+                      (I    : Integer := 0;
+                                    Incr : Integer := 1) return Integer is
+begin
+   return I + Incr;
+end increment_by;
