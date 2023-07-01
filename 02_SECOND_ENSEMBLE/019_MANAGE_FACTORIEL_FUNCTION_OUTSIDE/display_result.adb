f3e95948f9f913b8af4364cf48769692f6b105ee
Merzouk MENHOUR <merzouk@gmail.com>
2023-07-01 16:18:28 +0200

    update method

--- 02_SECOND_ENSEMBLE/019_MANAGE_FACTORIEL_FUNCTION_OUTSIDE/display_result.adb
+++ 02_SECOND_ENSEMBLE/019_MANAGE_FACTORIEL_FUNCTION_OUTSIDE/display_result.adb
@@ -1,17 +1,7 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
    
-   procedure display_result(a: integer; b : integer ) is
-   begin
-      Put_Line ( if (a <= 9) then 
-					"Factorial of "
-					& Integer'Image (a)
-					& "  is  "
-					& Integer'Image (b)
-				else
-					"Factorial of "
-					& Integer'Image (a)
-					& " is  "
-					& Integer'Image (b)
-				);
-   end display_result;
+    procedure display_result(message: string) is
+		begin
+			Put_Line (message);
+    end display_result;
--- 02_SECOND_ENSEMBLE/019_MANAGE_FACTORIEL_FUNCTION_OUTSIDE/manage_factoriel.adb
+++ 02_SECOND_ENSEMBLE/019_MANAGE_FACTORIEL_FUNCTION_OUTSIDE/manage_factoriel.adb
@@ -1,17 +1,13 @@
-
-with factorial;
 with saisi_limite;
-with display_result;
+
+with for_loop_calculate;
 
 procedure manage_factoriel is
    
-   C : Integer;
-   limit : Integer;
+   -- C : Integer;
+   -- limit : Integer;
 	
-begin
-    limit := saisi_limite(1);
-    for A in 1 .. limit loop
-	   C := factorial(A);
-	   display_result(A, C);
-	end loop;
+   begin
+      -- limit := saisi_limite(1);
+      for_loop_calculate(saisi_limite(1));
 end manage_factoriel;
