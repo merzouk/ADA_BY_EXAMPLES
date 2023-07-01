e440261fb67b74ca38126085949cf0972c6c9697
Merzouk MENHOUR <merzouk@gmail.com>
2023-07-01 16:35:41 +0200

    update method

--- 02_SECOND_ENSEMBLE/019_MANAGE_FACTORIEL_FUNCTION_OUTSIDE/factorial.adb
+++ 02_SECOND_ENSEMBLE/019_MANAGE_FACTORIEL_FUNCTION_OUTSIDE/factorial.adb
@@ -3,8 +3,9 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
    function factorial(f : Integer) return Integer is
    begin
-	   if(f = 0 xor f = 1) then return 1; end if;
-	   if(f > 1) then return (f*factorial(f-1)); end if;
-	   return 1;
+      if(f < 0) then return 1; end if;
+	   if(f = 0 xor f = 1) then return 1;
+	   else return (f*factorial(f-1)); end if;
+	   -- return 1;
    end factorial;
    
