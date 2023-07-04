feb1f139e050f7cec0d383fa44f6fc62f611b6b1
Mr MN <merzouk@gmail.com>
2023-07-04 23:40:00 +0200

    Update main.cpp

--- C_CPP_ADD/CALL_PROCEDURE_AND_FUNCTION_ADA_FROM_CPP_PROGRAM/main.cpp
+++ C_CPP_ADD/CALL_PROCEDURE_AND_FUNCTION_ADA_FROM_CPP_PROGRAM/main.cpp
@@ -13,10 +13,10 @@
 extern "C" 
 {
         void print_number (int data);
-		void print_factorial (int fac);
-		int saisi_limite (int limit);
-		int factorial (int fact);
-		int doubler (int d);
+	void print_factorial (int fac);
+	int saisi_limite (int limit);
+	int factorial (int fact);
+	int doubler (int d);
         void adainit ();
         void adafinal ();
 }
