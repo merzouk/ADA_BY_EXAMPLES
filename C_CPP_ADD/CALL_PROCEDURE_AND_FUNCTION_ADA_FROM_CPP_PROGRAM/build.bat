c8f0d3ec6e34c762b2d5e590010cbab030727ce5
Merzouk MENHOUR <merzouk@gmail.com>
2023-07-05 17:44:35 +0200

    add

---
+++ C_CPP_ADD/CALL_PROCEDURE_AND_FUNCTION_ADA_FROM_CPP_PROGRAM/build.bat
@@ -0,0 +1,31 @@
+echo "remove  all *.ali files"
+
+rm *.ali
+
+echo "remove all *.o files"
+rm *.o
+
+echo "remove exec file"
+
+rm main
+
+echo "run gnatmake" 
+
+gnatmake -c ada_package
+
+echo "run g++ command"
+
+g++ -g -Wall -ansi -pedantic -std=c++0x -c *.cpp
+
+
+echo "run gnatbind command"
+
+gnatbind -n ada_package
+
+
+echo "build exec "
+
+gnatlink ada_package -o main --LINK=g++ -lstdc++ main.o Calculs.o
+
+
+pause
