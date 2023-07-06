3673f8c1ca384103f6024a8514bff917d1e08e6b
Merzouk MENHOUR <merzouk@gmail.com>
2023-07-06 09:40:52 +0200

    update files

--- C_CPP_ADD/CALL_PROCEDURE_AND_FUNCTION_ADA_FROM_CPP_PROGRAM/ada_package.adb
+++ C_CPP_ADD/CALL_PROCEDURE_AND_FUNCTION_ADA_FROM_CPP_PROGRAM/ada_package.adb
@@ -1,6 +1,7 @@
 
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Exceptions;  use Ada.Exceptions;
 
 package body ada_package is
 
@@ -10,14 +11,33 @@ package body ada_package is
 		    if(fact = 0 xor fact = 1) then return 1;
 		    else return (fact*factorial(fact-1)); end if;
 	   end factorial;
+	
+	function gestion_saisie(limit : Integer) return Integer is
+	    N : Integer := 0;
+		begin
+		    Skip_Line(1);
+		    Ada.Text_Io.Put_Line("ADA : Saisir un entier non null inferieur ou egal a " & Integer'Image (limit) & " : ");
+			Get(N) ; 
+			return N;
+		exception
+			when Data_Error =>
+							Ada.Text_IO.Put_Line ("La saisie n'est pas correcte ");
+							return 0;
+			when Constraint_Error =>
+							Ada.Text_IO.Put_Line ("La saisie n'est pas correcte ");
+							return 0;
+			when others =>
+							Ada.Text_IO.Put_Line ("La saisie n'est pas correcte ");
+							return 0;
+	end gestion_saisie;
+
 	   
 	function saisi_limite(limit : in Integer) return Integer is
 		N : integer := 0;
 		begin
 				loop
 					N := 0;
-					Ada.Text_Io.Put_Line("ADA : Saisir un entier non null inferieur ou egal a " & Integer'Image (limit) & " : ");
-					Get(N) ; Skip_line ; 
+					N := gestion_saisie(limit);
 					if (N > 0 and N <= limit) then return N; end if ; 
 				end loop ; 
 		end saisi_limite;
