f2f174c02ea8b3d2e3e90059105fe2782986d602
Merzouk MENHOUR <merzouk@gmail.com>
2023-07-14 13:20:52 +0200

    add example

---
+++ build.bat
@@ -0,0 +1,20 @@
+echo "Start generating the executable file"
+
+echo "run gnatmake" 
+
+gnatmake -c ada_package
+
+echo "run g++ command"
+
+g++ -g -Wall -ansi -pedantic -std=c++0x -c *.cpp
+
+
+echo "run gnatbind command"
+
+gnatbind -n ada_package
+
+
+echo "build exec "
+
+gnatlink ada_package -o main --LINK=g++ -lstdc++ main.o Calculs.o
+
