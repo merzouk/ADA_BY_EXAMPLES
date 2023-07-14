3b6681b1032df8a8d5244c67517c0e9f8211c208
Merzouk MENHOUR <merzouk@gmail.com>
2023-07-15 01:31:48 +0200

    add example

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Start generating the executable file"
+
+echo "run gnatmake" 
+gnatmake -c ada_package
+
+echo "run g++ command"
+g++ -g -Wall -ansi -pedantic -std=c++0x -c *.cpp
+
+
+echo "run gnatbind command"
+gnatbind -n ada_package
+
+echo "build exec"
+gnatlink ada_package -o main --LINK=g++ -lstdc++ main.o Calculs.o
+
+echo "End generating the executable file"
+
+exit 0
