f17fa6fdbde6eed9baa51ad540e0db1e7159316c
Merzouk MENHOUR <merzouk@gmail.com>
2023-07-15 16:59:31 +0200

    update

--- C_CPP_ADD/MANAGE_FACTORIEL_FUNCTION_OUTSIDE/clean.sh
+++ C_CPP_ADD/MANAGE_FACTORIEL_FUNCTION_OUTSIDE/clean.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 echo "begin file cleaning"
+if [ -e *.ali ]; then 
+	echo "remove  all *.ali files"
+	rm *.ali
+fi
 
-echo "remove  all *.ali files"
+if [ -e *.o ]; then 
+	echo "remove all *.o files"
+	rm *.o
+fi
 
-rm *.ali
-
-echo "remove all *.o files"
-
-rm *.o
-
-echo "remove exec file"
-
-rm main
+if [ -e main ]; then 
+	echo "remove exec file"
+	rm main
+fi
 
 echo "end file cleaning"
 
