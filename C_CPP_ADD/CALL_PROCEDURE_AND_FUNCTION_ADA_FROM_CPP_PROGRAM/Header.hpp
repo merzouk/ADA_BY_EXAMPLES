6131f89fe413c6869243ac7ef4d48926392f32cc
merzouk menhour <merzouk@gmail.com>
2023-08-25 17:58:14 +0200

    add

--- C_CPP_ADD/CALL_PROCEDURE_AND_FUNCTION_ADA_FROM_CPP_PROGRAM/Header.hpp
+++ C_CPP_ADD/CALL_PROCEDURE_AND_FUNCTION_ADA_FROM_CPP_PROGRAM/Header.hpp
@@ -6,7 +6,7 @@ using namespace std;
 
 extern "C" 
 {
-    void print_number (int data) ;
+    void print_number (int data);
 	void print_factorial (int fac);
 	int saisi_limite (int limit);
 	int factorial (int fact);
